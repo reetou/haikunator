5ffbfe30287c9143b2b8b152103283974dfbf6d2
reetou <fillmasterson@gmail.com>
2020-04-18 21:39:27 +0300

    Add words

--- config/adjectives.exs
+++ config/adjectives.exs
@@ -1,12 +1,17 @@
 use Mix.Config
 
 config :haikunator, :adjectives, ~w(
-  autumn hidden bitter misty silent empty dry dark summer
-  icy delicate quiet white cool spring winter patient
-  twilight dawn crimson wispy weathered blue billowing
-  broken cold damp falling frosty green long late lingering
-  bold little morning muddy old red rough still small
-  sparkling throbbing shy wandering withered wild black
-  young holy solitary fragrant aged snowy proud floral
-  restless divine polished ancient purple lively nameless
+  золотая летняя темная сухая чайная кофейная
+  красная желтая цветная синяя белая
+  красивая чумовая лесная радужная сонная холодная
+  блинная водяная горькая сладкая кислая летящая
+  бледная румяная ручная пивная удалая
+  горячая голубая розовая зеленая грибная
+  ягодная занятая деловая жаркая жгучая
+  меховая дикая добрая озорная громкая
+  идеальная крайняя рядовая неземная
+  немалая большая великая острая пущая
+  прямая рьяная сильная стойкая яркая
+  суровая сущая строгая тяжкая ярая шумная
+  щедрая щемящая
 )
--- config/nouns.exs
+++ config/nouns.exs
@@ -1,12 +1,16 @@
 use Mix.Config
 
 config :haikunator, :nouns, ~w(
-   waterfall river breeze moon rain wind sea morning
-   snow lake sunset pine shadow leaf dawn glitter forest
-   hill cloud meadow sun glade bird brook butterfly
-   bush dew dust field fire flower firefly feather grass
-   haze mountain night pond darkness snowflake silence
-   sound sky shape surf thunder violet water wildflower
-   wave water resonance sun wood dream cherry tree fog
-   frost voice paper frog smoke star
+  птица белка чайка акула
+  змейка сорока цивета калао
+  ябиру шемая акара алекта
+  алоза муния галка ласка
+  даллия амейва амека лисица
+  норка оляпка палия савка
+  синьга щука протея амия
+  львица утка соня андива
+  кошка ани тинаму аноа
+  арама пчела аттила афарея
+  рысь акария гну ворона
+  афина бария агния
 )
